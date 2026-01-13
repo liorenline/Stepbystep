ab286f4aad27cb1f4e7d7d2410d41eb8ccfb28f2
Avtobu <lifeivanna2007@gmail.com>
2026-01-13 14:45:57 +0200

    Checking in password

--- web/auth.py
+++ web/auth.py
@@ -2,8 +2,7 @@ from flask import Blueprint, request, flash, redirect, url_for
 from . import db
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_manager, login_required, logout_user, current_user
-
+from flask_login import login_user, login_required, logout_user
 
 auth = Blueprint('auth', __name__)
 
@@ -30,34 +29,51 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('views.home'))
+    return redirect(url_for('auth.login'))
 
-@auth.route("/sign-up", methods = ['GET', 'POST'])
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
-    if request.method=='POST':
+    if request.method == 'POST':
         email = request.form.get('email')
         firstName = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         user = User.query.filter_by(email=email).first()
+
+        has_upp = has_sp = has_num = False
+        for c in password1:
+            if c.isupper():
+                has_upp = True
+            elif c.isdigit():
+                has_num = True
+            elif not c.isalnum():
+                has_sp = True
+
         if user:
             flash('Email already exists', category='error')
-
-    if len(email) <4:
-        flash ('Email is too short', category='eror')
-    elif len(firstName) < 2:
-        flash('Name is too short', category = 'error')
-    elif len(password1)< 8:
-        flash('Password is too short', category='error')
-    elif password1 != password2:
-        flash('Confirm password is differ, try again', category='error')
-    else:
-        new_user = User(email=email, firstName= firstName, password = generate_password_hash(password1, method='sha256'))
-        db.session.add(new_user)
-        db.session.commit()
-        login_user(new_user, remember=True)
-        flash('Account created!', category = 'success')
-        return redirect(url_for('views.home'))
+        elif len(email) < 4:
+            flash('Email is too short', category='error')
+        elif len(firstName) < 2:
+            flash('Name is too short', category='error')
+        elif len(password1) < 8:
+            flash('Password is too short', category='error')
+        elif not has_upp:
+            flash('Password has no upper letters', category='error')
+        elif not has_num:
+            flash('Password has no numbers', category='error')
+        elif not has_sp:
+            flash('Password has no special characters', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')
+        if not email or not firstName or not password1 or not password2:
+            flash('All fields must be filled', category='error')
+        else:
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return "sign-up"
